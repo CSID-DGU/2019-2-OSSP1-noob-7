cdee62bee45ae75d35537c613a0671026ee7a142
juliaha <juliaha9709@icloud.com>
2019-10-11 23:36:39 +0900

    create id match

--- android/app/src/main/java/com/example/tutorial1/MainActivity.java
+++ android/app/src/main/java/com/example/tutorial1/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.tutorial1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText input_email, input_pw;
+    Button login_button, signup_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        input_email = findViewById(R.id.input_email);
+        input_pw = findViewById(R.id.input_pw);
+        login_button = findViewById(R.id.login_button);
+        signup_button = findViewById(R.id.signup_button);
     }
 }
