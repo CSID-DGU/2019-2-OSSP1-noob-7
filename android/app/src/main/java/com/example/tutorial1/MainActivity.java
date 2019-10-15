741d3d63f473b0acbd63fc956ad53b3180cbe295
Youngmin <aabb118@naver.com>
2019-10-15 23:24:02 +0900

    reset

--- android/app/src/main/java/com/example/tutorial1/MainActivity.java
+++ android/app/src/main/java/com/example/tutorial1/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.tutorial1;
 
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,19 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText emailID, password;
     Button login_button, signup_button;
 
-    FirebaseAuth mFirebaseAuth;
 
 
     @Override
@@ -49,46 +42,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String email = emailID.getText().toString();    // 이메일 받은 걸 저장
-                String pwd = password.getText().toString();     // 비밀번호 받은 걸 저장
-
-                // 이메일 또는 비밀번호 입력 안할 시 에러 알림
-                if(email.isEmpty()){
-                    emailID.setError("Please enter your e-mail");
-                    emailID.requestFocus();
-
-                }
-                else if(pwd.isEmpty()){
-                    password.setError("Please enter your password");
-
-                }
-                // 이메일과 비밀번호 둘 다 비어있을 경우:
-                else if(email.isEmpty() && pwd.isEmpty()){
-                    Toast.makeText(MainActivity.this,"Fields are empty!",Toast.LENGTH_SHORT).show();
-
-                }
-                else if(!(email.isEmpty() && pwd.isEmpty())){
-                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(!task.isSuccessful()){
-
-
-                            }
-
-                        }
-                    });
-
-                }
 
                 Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
 
                 startActivity(intent);
+
             }
         });
 
-
-        mFirebaseAuth = FirebaseAuth.getInstance();
 
         emailID = findViewById(R.id.input_email);
         password = findViewById(R.id.input_pw);
