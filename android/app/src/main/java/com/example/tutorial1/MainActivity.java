7b5f0782de4e012fad01e60165ed202a0a9eea1b
Youngmin <aabb118@naver.com>
2019-10-16 23:02:05 +0900

    registration & login

--- android/app/src/main/java/com/example/tutorial1/MainActivity.java
+++ android/app/src/main/java/com/example/tutorial1/MainActivity.java
@@ -8,12 +8,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText emailID, password;
-    Button login_button, signup_button;
+    EditText et_id, et_pass;
+    Button btn_login, btn_register;
 
 
 
@@ -22,23 +30,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        btn_login = findViewById(R.id.btn_login);
+        btn_register = findViewById(R.id.btn_register);
 
 
-        login_button = (Button)findViewById(R.id.login_button);
+        // 로그인 버튼을 클릭 시 수행
 
-        login_button.setOnClickListener(new View.OnClickListener() {
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, MainDisplayActivity.class);
+                // EditText에 현재 입력되어있는 값을 get 해온다.
+                String userID = et_id.getText().toString();
+                String userPass = et_pass.getText().toString();
 
-                startActivity(intent);
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+
+                            if (success){ // 로그인에 성공한 경우
+                                String userID = jsonObject.getString("userID");
+                                String userPass = jsonObject.getString("userPassword");
+
+                                Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+
+                                // 아이디와 비밀번호를 받아서 메인으로 넘긴다.
+                                Intent intent = new Intent(MainActivity.this, MainDisplayActivity.class);
+                                intent.putExtra("userID",userID);
+                                intent.putExtra("userPass",userPass);
+                                startActivity(intent);
+
+                            }
+                            else { // 로그인에 실패한 경우
+                                Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                                return;
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                };
+
+               LoginRequest loginRequest = new LoginRequest(userID, userPass, responseListener);
+               RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+               queue.add(loginRequest);
             }
         });
 
-        signup_button = (Button)findViewById(R.id.signup_button);
 
-        signup_button.setOnClickListener(new View.OnClickListener() {
+        // 회원가입 버튼을 클릭 시 수행(회원가입 창 이동)
+
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -51,8 +104,6 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        emailID = findViewById(R.id.input_email);
-        password = findViewById(R.id.input_pw);
 
     }
 }
--- android/app/src/main/java/com/example/tutorial1/MainDisplayActivity.java
+++ android/app/src/main/java/com/example/tutorial1/MainDisplayActivity.java
@@ -1,6 +1,8 @@
 package com.example.tutorial1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +13,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainDisplayActivity extends AppCompatActivity {
 
+    private TextView tv_id, tv_pass;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        tv_id = findViewById(R.id.tv_id);
+        tv_pass = findViewById(R.id.tv_pass);
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPass = intent.getStringExtra("userPass");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPass);
     }
 }
