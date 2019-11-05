b20b5473a4fcab95c335ad9fbbb561185036dcaa
Youngmin <aabb118@naver.com>
2019-11-05 15:01:43 +0900

    edited chart fragment

--- android/app/src/main/java/com/example/tutorial1/ChartFragment.java
+++ android/app/src/main/java/com/example/tutorial1/ChartFragment.java
@@ -9,12 +9,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ChartFragment extends Fragment {
 
+
+    private BarChart mBarChart;
+    private PieChart mPieChart;
 
     public ChartFragment() {
         // Required empty public constructor
@@ -25,7 +37,52 @@ public class ChartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false);
+        View view = inflater.inflate(R.layout.fragment_chart, container, false);
+
+        mBarChart = view.findViewById(R.id.barChart);
+        mPieChart = view.findViewById(R.id.pieChart);
+
+        getGrowthChart(getArguments().getString("method"));
+
+        return view;
+    }
+
+    private void getGrowthChart(final String method)
+    {
+        Call<List<GraphActivity>> call = ApiClient.getApiClient().create(ApiInterface.class).getGrowthInfo();
+
+
+        call.enqueue(new Callback<List<GraphActivity>>() {
+            @Override
+            public void onResponse(Call<List<GraphActivity>> call, Response<List<GraphActivity>> response) {
+
+                if(response.body()!=null)
+                {
+                    if(method.equals("bars"))
+                    {
+
+
+                    }
+
+                    else if(method.equals("pie"))
+                    {
+
+
+                    }
+
+                }
+
+
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<GraphActivity>> call, Throwable t) {
+
+            }
+        });
+
     }
 
 }
