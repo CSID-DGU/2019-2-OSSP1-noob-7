2aade0ca4a6d5d9ba78913398f10261d8682f2b9
Youngmin <aabb118@naver.com>
2019-11-06 11:04:14 +0900

    edited chart activity

--- android/app/src/main/java/com/example/tutorial1/ChartActivity.java
+++ android/app/src/main/java/com/example/tutorial1/ChartActivity.java
@@ -11,6 +11,14 @@ public class ChartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chart);
 
-        
+        String method = getIntent().getStringExtra("method");
+        ChartFragment chartFragment = new ChartFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("method",method);
+        chartFragment.setArguments(bundle);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,chartFragment).commit();
+
     }
 }
