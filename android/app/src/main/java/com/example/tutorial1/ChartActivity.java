e5e84c0fd4d8106c0350ae189a9c78127dd2cfbc
Youngmin <aabb118@naver.com>
2019-11-06 11:01:42 +0900

    added chart activity

---
+++ android/app/src/main/java/com/example/tutorial1/ChartActivity.java
@@ -0,0 +1,16 @@
+package com.example.tutorial1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+
+        
+    }
+}
