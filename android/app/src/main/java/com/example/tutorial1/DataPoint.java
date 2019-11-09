369f69d4edd1abd6f9e25a5b1ef7ac6064ae310e
Youngmin <aabb118@naver.com>
2019-11-09 10:41:40 +0900

    created data point

---
+++ android/app/src/main/java/com/example/tutorial1/DataPoint.java
@@ -0,0 +1,22 @@
+package com.example.tutorial1;
+
+public class DataPoint {
+
+    int xValue, yValue;
+
+    public DataPoint(int xValue, int yValue) {
+        this.xValue = xValue;
+        this.yValue = yValue;
+    }
+
+    public DataPoint() {
+    }
+
+    public int getxValue() {
+        return xValue;
+    }
+
+    public int getyValue() {
+        return yValue;
+    }
+}
