020c2fc9d71d6ac185c85c5dd48c8af86bf2c260
Youngmin <aabb118@naver.com>
2019-11-18 21:28:25 +0900

    View Holder

--- android/app/src/main/java/com/example/tutorial1/RecyclerViewAdapter.java
+++ android/app/src/main/java/com/example/tutorial1/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.tutorial1;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -9,9 +10,25 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
 
     private static final String TAG = "RecyclerViewAdapter";
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
@@ -24,6 +41,10 @@ public class RecyclerViewAdapter {
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.image_name);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
         }
     }
 }
