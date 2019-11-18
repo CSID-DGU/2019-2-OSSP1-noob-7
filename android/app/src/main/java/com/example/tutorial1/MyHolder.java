7eb8838dd98e5238abf8cb32760e6e9913c383a9
Youngmin <aabb118@naver.com>
2019-11-18 15:28:54 +0900

    Created Holder

---
+++ android/app/src/main/java/com/example/tutorial1/MyHolder.java
@@ -0,0 +1,16 @@
+package com.example.tutorial1;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyHolder extends RecyclerView.ViewHolder {
+
+    
+
+
+    public MyHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
