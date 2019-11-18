dc91e7a3ca580a254ba2f5a2e09628a99f54b09b
Youngmin <aabb118@naver.com>
2019-11-18 15:36:05 +0900

    Created Adapter class

---
+++ android/app/src/main/java/com/example/tutorial1/MyAdapter.java
@@ -0,0 +1,26 @@
+package com.example.tutorial1;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyHolder> {
+
+    
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
