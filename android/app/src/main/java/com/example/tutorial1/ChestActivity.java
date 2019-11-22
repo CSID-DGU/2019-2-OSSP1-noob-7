aa6a390ce86fe605b84e4cd622e534d976427da3
Youngmin <aabb118@naver.com>
2019-11-22 12:21:10 +0900

    youtube initialization

--- android/app/src/main/java/com/example/tutorial1/ChestActivity.java
+++ android/app/src/main/java/com/example/tutorial1/ChestActivity.java
@@ -5,11 +5,35 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class ChestActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class ChestActivity extends YouTubeBaseActivity {
+
+    private final String API_KEY = "AIzaSyCvXeZ8vj_n9Qki2iqw_b3W2RW_ClDMujk";
+    private final String VIDEO_CODE = "Cse56EbcBSk";
+
+    YouTubePlayerView player;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chest);
+
+
+        player = (YouTubePlayerView)findViewById(R.id.player);
+        player.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        });
     }
 }
