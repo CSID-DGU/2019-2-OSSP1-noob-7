3078f07401c46e6bd806a8123588d8c10edbdb3d
Youngmin <aabb118@naver.com>
2019-11-25 15:41:38 +0900

    created journal activity

---
+++ android/app/src/main/java/com/example/tutorial1/WorkoutJournalActivity.java
@@ -0,0 +1,15 @@
+package com.example.tutorial1;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WorkoutJournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal);
+    }
+}
