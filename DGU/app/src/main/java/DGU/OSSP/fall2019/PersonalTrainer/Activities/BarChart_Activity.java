f4cb38523fa06acf80fff1064f56dd9a0d9b8c26
Youngmin <aabb118@naver.com>
2019-12-16 15:04:23 +0900

    UI

--- DGU/app/src/main/java/DGU/OSSP/fall2019/PersonalTrainer/Activities/BarChart_Activity.java
+++ DGU/app/src/main/java/DGU/OSSP/fall2019/PersonalTrainer/Activities/BarChart_Activity.java
@@ -1,11 +1,12 @@
 package DGU.OSSP.fall2019.PersonalTrainer.Activities;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-
+import android.view.View;
+import android.widget.Button;
 
 
 import com.github.mikephil.charting.charts.BarChart;
@@ -26,6 +27,10 @@ public class BarChart_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar_chart);
 
+        Button btn_Meal;
+
+        btn_Meal = findViewById(R.id.btnMeal);
+
         barChart = findViewById(R.id.mp_BarChart);
 
         String strColor = "#004489";
@@ -39,19 +44,30 @@ public class BarChart_Activity extends AppCompatActivity {
         barChart.setData(barData);
         barChart.invalidate();
 
+        btn_Meal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(BarChart_Activity.this, MainActivity.class);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 
     private ArrayList<BarEntry> dataValues1(){
 
         ArrayList<BarEntry> dataVals = new ArrayList<>();
 
-        dataVals.add(new BarEntry(1, 120));
-        dataVals.add(new BarEntry(2, 110));
-        dataVals.add(new BarEntry(3, 90));
-        dataVals.add(new BarEntry(4, 85));
-        dataVals.add(new BarEntry(5, 80));
-        dataVals.add(new BarEntry(6, 60));
-        dataVals.add(new BarEntry(7, 50));
+        dataVals.add(new BarEntry(1, 100));
+        dataVals.add(new BarEntry(2, 90));
+//        dataVals.add(new BarEntry(3, 80));
+//        dataVals.add(new BarEntry(4, 70));
+//        dataVals.add(new BarEntry(5, 80));
+//        dataVals.add(new BarEntry(6, 60));
+//        dataVals.add(new BarEntry(7, 50));
 
         return dataVals;
 
